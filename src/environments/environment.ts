e17d50ac970a5442bdb1f683f7c5c4de8a0fa648
Fabio Iwakoshi <fabio.iwakoshi@gmail.com>
2020-01-17 14:51:17 -0300

    core: scss and Firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCrLkogRb-vqbquvw6usvi3dEEmCnndaC4',
+    authDomain: 'iwakoshi-todo.firebaseapp.com',
+    databaseURL: 'https://iwakoshi-todo.firebaseio.com',
+    projectId: 'iwakoshi-todo',
+    storageBucket: 'iwakoshi-todo.appspot.com',
+    messagingSenderId: '500325140056',
+    appId: '1:500325140056:web:95386e83a5559d8b1cac2d',
+    measurementId: 'G-SF5YC1EEQR'
+  }
 };
 
 /*
